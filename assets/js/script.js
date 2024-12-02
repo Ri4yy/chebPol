e219420fb153b7da8d78cfed607372c259e7bdd8
Ri4yy <maksimov.1913@list.ru>
2024-12-02 10:19:04 +0300

    comit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     let menu = document.querySelector('.menu'),
-    btnMenu = document.querySelector('.btn-menu');
+    btnMenu = document.querySelector('.btn-menu'),
+    html = document.querySelector('html');
 
     btnMenu.addEventListener('click', (e) => {
         menu.classList.toggle('menu--open')
 
         btnMenu.classList.toggle('btn-menu--open')
+        html.classList.toggle('no-scroll')
     })
 
     function resize() {
@@ -23,4 +25,39 @@ document.addEventListener('DOMContentLoaded', () => {
         resize()
     })
     resize()
+
+    $.mask.definitions['h'] = "[0|1|3|4|5|6|7|9]";
+    $("input[data-phone]").mask("+7 (h99) 999-99-99");
+
+    // Модальное окно
+    function showModal(btnOpen, modalBody) {
+        btnOpen.click(function () {
+            modalBody.addClass('active');
+            $('html').addClass('no-scroll');
+            return false;
+        });
+
+        $(document).keydown(function (e) {
+            if (e.keyCode === 27) {
+                e.stopPropagation();
+                modalBody.removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        modalBody.click(function (e) {
+            if ($(e.target).closest('.modal__wrapper').length == 0) {
+                $(this).removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        $('.close-modal').click((e) => {
+            modalBody.removeClass('active');
+            $('html').removeClass('no-scroll');
+        })
+    }
+
+    showModal($('.btn-consult'), $('.modal--form'));
+    showModal($('.success'), $('.modal--success'));
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,49 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 20,
-    enabled: true,
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper-certificate', {
 
-    breakpoints: {
-        320: {
-            allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+        initialSlide: 2,
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 5,
+            slideShadows: false
         },
-        480: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+
+        breakpoints: {
+            320: {
+                allowTouchMove: true,
+                slidesPerView: 1,
+                initialSlide: 0,
+            },
+            480: {
+                allowTouchMove: true,
+                slidesPerView: 2.5
+            },
+            768: {
+                allowTouchMove: true,
+                slidesPerView: 3
+            },
+            1024: {
+                allowTouchMove: true,
+                slidesPerView: 3
+            },
+            1280: {
+                allowTouchMove: true,
+                slidesPerView: 3
+            },
+            1921: {
+                allowTouchMove: true,
+                slidesPerView: 4
+            },
         },
-        768: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+        
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        1024: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1280: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+    });
+})
